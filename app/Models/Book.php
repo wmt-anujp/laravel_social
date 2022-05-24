d5c98d4df6ea451c12b55a6749fce59bcec984bd
anuj panchal <anujp@webmobtech.com>
2022-05-24 10:40:10 +0530

    book image mutator

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,11 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\Request;
 
 class Book extends Model
 {
     use HasFactory;
     public $imagefolder = "bookimg/";
+    // public $imagefolder = asset(Storage::disk("local")->url("bookimg/"));
+
     public function authors()
     {
         return $this->belongsToMany(Author::class);
@@ -17,6 +21,8 @@ class Book extends Model
     // Mutators
     public function setBookImageAttribute($image)
     {
+        // $imagefolder = $request->move(public_path('bookimg'));
+        // $this->imagefolder = Storage::disk(['local'])->url("bookimg/");
         $this->attributes['book_image'] = $this->imagefolder . $image;
     }
 }
