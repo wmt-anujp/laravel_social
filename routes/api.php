ffa9d92d6d16b4a332045e93d212fcc530612d2d
anuj panchal <anujp@webmobtech.com>
2022-08-08 09:47:23 +0530

    api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\Auth\AdminController;
+use App\Http\Controllers\User\Auth\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('userlogin', function () {
+    return response()->json([
+        'name' => 'anuj',
+        'surname' => 'panchal',
+    ]);
+});
+
+Route::post('user-register', [UserController::class, 'userSignup']);
+Route::post('admin-login', [AdminController::class, 'adminLogin']);
